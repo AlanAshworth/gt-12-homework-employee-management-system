d084ca8a51423db09a475bb873d77b6445abdf39
Alan Ashworth <awashworth927@gmail.com>
2020-04-07 18:03:56 -0400

    Add view options to tracker. Modify quit method to end connection, then followed with break.

--- employeeTrackerCLI.js
+++ employeeTrackerCLI.js
@@ -1,91 +1,152 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var cTable = require("console.table");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 var connection = mysql.createConnection({
-    host:"localhost",
-    port:3306,
-    user: "root",
-    password:"root",
-    database:"employee_tracker_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "employee_tracker_db",
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    runDatabase();
+connection.connect(function (err) {
+  if (err) throw err;
+  runDatabase();
 });
 
 function runDatabase() {
-    inquirer
+  inquirer
     .prompt({
       name: "action",
       type: "rawlist",
-      message: "Welcome to Employee Tracker. Select an action.",
-      choices: [
-        "View Table data",
-        "Add Table Data",
-        "Update Table Data"
-      ]
+      message: "Select an action for your data.",
+      choices: ["View", "Add", "Update", "Quit"],
     })
-    .then(function(answer) {
+    .then(function (answer) {
       switch (answer.action) {
-      case "View Table Data":
-        viewTableData();
-        break;
-      case "Add Table Data":
-        addTableData();
-        break;
-      case "Update Table Data":
-        updateTableData();
-        break;
-      default:
-        proceedDirection();
+        case "View":
+          viewTableData();
+          break;
+        case "Add":
+          addTableData();
+          break;
+        case "Update":
+          updateTableData();
+          break;
+        case "Quit":
+          quit();
+          break;
+        default:
+          throw new Error("Invalid selection.");
       }
     });
 }
 
-function viewTableData(params) {
-    console.log("Proceeding to View Table Data menu.");
-}
-
-function addTableData(params) {
-    console.log("Proceeding to Add Table Data menu.");
-}
-
-function updateTableData(params) {
-    console.log("Proceeding to Update Table Data menu.");
-}
-
-function proceedDirection(params) {
-    inquirer
+function viewTableData() {
+  inquirer
     .prompt({
-      name: "proceed",
+      name: "view",
       type: "list",
-      message: "Retry or Quit?",
-      choices: [
-        "Retry",
-        "Quit"
-      ]
+      message: "Select a table to view data.",
+      choices: ["Role", "Employee", "Department", "Go Back"],
     })
-    .then(function(answer) {
-      switch (answer.proceed) {
-      case "Retry":
-        retry();
-        break;
-      case "Quit":
-        quit();
-        break;
-      default:
-        throw new Error("Invalid selection.");
+    .then(function (answer) {
+      switch (answer.view) {
+        case "Role":
+          viewRoleTable();
+          break;
+        case "Employee":
+          viewEmployeeTable();
+          break;
+        case "Department":
+          viewDepartmentTable();
+          break;
+        case "Go Back":
+          runDatabase();
+        default:
+          throw new Error("Invalid selection.");
       }
     });
+
+  function viewRoleTable() {
+    var query = "SELECT * FROM role;";
+    connection.query(query, function (err, res) {
+      if (err) {
+        throw err;
+      } else {
+        let roles = [];
+        for (let i = 0; i < res.length; i++) {
+          let element = {
+            ID: `${res[i].id}`,
+            Title: `${res[i].title}`,
+            Salary: `${res[i].salary}`,
+            DeptID: `${res[i].department_id}`
+          };
+          roles.push(element);
+        }
+        console.table(roles);
+        roles.length = 0;
+      }
+      runDatabase();
+    });
+  }
+
+  function viewEmployeeTable() {
+    var query = "SELECT * FROM employee;";
+    connection.query(query, function (err, res) {
+      if (err) {
+        throw err;
+      } else {
+        let employees = [];
+        for (let i = 0; i < res.length; i++) {
+          let element = {
+            ID: `${res[i].id}`,
+            FirstName: `${res[i].first_name}`,
+            LastName: `${res[i].last_name}`,
+            RoleID: `${res[i].role_id}`,
+            ManagerID: `${res[i].manager_id}`
+          };
+          employees.push(element);
+        }
+        console.table(employees);
+        employees.length = 0;
+      }
+      runDatabase();
+    });
+  }
+
+  function viewDepartmentTable() {
+    var query = "SELECT * FROM department;";
+    connection.query(query, function (err, res) {
+      if (err) {
+        throw err;
+      } else {
+        let departments = [];
+        for (let i = 0; i < res.length; i++) {
+          let element = {
+            ID: `${res[i].id}`,
+            Name: `${res[i].name}`
+          };
+          departments.push(element);
+        }
+        console.table(departments);
+        departments.length = 0;
+      }
+      runDatabase();
+    });
+  }
 }
 
-function retry() {
-    runDatabase();
+function addTableData() {
+  console.log("Proceeding to Add Table Data menu.");
 }
 
-function quit(params) {
-    console.log("Exiting Employee Tracker.");
-    break;
+function updateTableData() {
+  console.log("Proceeding to Update Table Data menu.");
+}
+
+function quit() {
+  console.log("Exiting Employee Tracker.");
+  connection.end();
 }
