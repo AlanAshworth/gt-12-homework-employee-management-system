43829a9a859e4f1f3bce98dea83b7838763cb4c0
Alan Ashworth <awashworth927@gmail.com>
2020-04-07 15:31:19 -0400

    Add basic outline to menu system.

--- employeeTrackerCLI.js
+++ employeeTrackerCLI.js
@@ -12,9 +12,80 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    searchDatabase();
+    runDatabase();
 });
 
-function searchDatabase() {
-    
+function runDatabase() {
+    inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "Welcome to Employee Tracker. Select an action.",
+      choices: [
+        "View Table data",
+        "Add Table Data",
+        "Update Table Data"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "View Table Data":
+        viewTableData();
+        break;
+      case "Add Table Data":
+        addTableData();
+        break;
+      case "Update Table Data":
+        updateTableData();
+        break;
+      default:
+        proceedDirection();
+      }
+    });
+}
+
+function viewTableData(params) {
+    console.log("Proceeding to View Table Data menu.");
+}
+
+function addTableData(params) {
+    console.log("Proceeding to Add Table Data menu.");
+}
+
+function updateTableData(params) {
+    console.log("Proceeding to Update Table Data menu.");
+}
+
+function proceedDirection(params) {
+    inquirer
+    .prompt({
+      name: "proceed",
+      type: "list",
+      message: "Retry or Quit?",
+      choices: [
+        "Retry",
+        "Quit"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.proceed) {
+      case "Retry":
+        retry();
+        break;
+      case "Quit":
+        quit();
+        break;
+      default:
+        throw new Error("Invalid selection.");
+      }
+    });
+}
+
+function retry() {
+    runDatabase();
+}
+
+function quit(params) {
+    console.log("Exiting Employee Tracker.");
+    break;
 }
